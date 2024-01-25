dde1ddfe0e91a107d4452b218b28ed01d6d28fe5
Mateusz Szczepański <matiszczep@gmail.com>
2024-01-25 19:59:41 +0100

    window rename and screenshots upload

--- deRARtizator/MainWindow.xaml.cs
+++ deRARtizator/MainWindow.xaml.cs
@@ -12,7 +12,7 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
     }
-        private void SelectFileButton_Click(object sender, RoutedEventArgs e)
+        private void SelectFileButton_Click(object sender, RoutedEventArgs e) 
     {
         var openFileDialog = new OpenFileDialog
         {
